86d59a5fb0bdf92c4241f500d8f218192ac13df2
Andrei Stefan <102211437+Picred@users.noreply.github.com>
2024-05-25 13:47:15 +0000

    feat: init exercise terminal js

--- terminal-linux-operative/assets/js/script.js
+++ terminal-linux-operative/assets/js/script.js
@@ -1,20 +1,39 @@
 (() => {    
     window.onload = () => {
+        
         terminal = {
-            // terminalElement : document.querySelector(".desktop>.wallpaper>.terminal"),
-            // navbarElement : document.querySelector(".desktop>.wallpaper>.terminal>.terminal-navbar"),
-            inputContainerElement : document.querySelector(".desktop>.wallpaper>.terminal>.input-shell"),
-            init : function() {
-                const row = document.createElement("input")
-                row.classList = ["row"];
-                this.inputContainerElement.innerHTML = "<span>andrei@ubuntu:$ </span>";
-                this.inputContainerElement.appendChild(row);
+            inputContainerElement : document.querySelector(".desktop>.wallpaper>.terminal>.input-container"),
+
+            init: function(){
+                const spanElement = document.createElement("span");
+                spanElement.classList = ["username"];
+                spanElement.innerHTML = "andrei@ubuntu~$: ";
+
+                const inputElement = document.createElement("input");
+                inputElement.type = "text";
+                this.inputContainerElement.appendChild(spanElement);
+                this.inputContainerElement.appendChild(inputElement);
+
+                inputElement.addEventListener("keydown", (event) => {
+                    if (event.key === "Enter"){
+                        const cmd = inputElement.value;
+                        this.persist(cmd);
+                    }
+                })
+            },
+
+            persist: function(cmd){
+                console.log(cmd)
+                const parent = this.inputContainerElement;
+                const oldchild = this.inputContainerElement.querySelector("input")
+                parent.removeChild(oldchild)
+                // this.init()
             }
         };
-        
+
         terminal.init()
-        console.log(terminal.inputContainerElement)
+        // console.log(terminal.inputContainerElement)
         // inizializza la shell
     }
-    
+
 })()
