a3fb11aed3d8f02ea5cca3ded87603bf905542fa
Andrei Stefan <102211437+Picred@users.noreply.github.com>
2024-05-26 10:56:08 +0000

    feat: add commands

--- terminal-linux-operative/assets/js/script.js
+++ terminal-linux-operative/assets/js/script.js
@@ -3,37 +3,110 @@
         
         terminal = {
             inputContainerElement : document.querySelector(".desktop>.wallpaper>.terminal>.input-container"),
-
-            init: function(){
+            username : "andrei",
+            init: function(username){
+                if (username !== undefined){
+                    this.username = username;
+                }
                 const spanElement = document.createElement("span");
                 spanElement.classList = ["username"];
-                spanElement.innerHTML = "andrei@ubuntu~$: ";
+                spanElement.innerHTML = `${this.username}@ubuntu~$:`;
 
                 const inputElement = document.createElement("input");
                 inputElement.type = "text";
+
                 this.inputContainerElement.appendChild(spanElement);
                 this.inputContainerElement.appendChild(inputElement);
+                inputElement.focus();
 
                 inputElement.addEventListener("keydown", (event) => {
                     if (event.key === "Enter"){
-                        const cmd = inputElement.value;
-                        this.persist(cmd);
+                        this.persist(inputElement.value);
                     }
                 })
             },
 
             persist: function(cmd){
-                console.log(cmd)
                 const parent = this.inputContainerElement;
-                const oldchild = this.inputContainerElement.querySelector("input")
-                parent.removeChild(oldchild)
-                // this.init()
+
+                this.removeChild();
+
+                parent.innerHTML += cmd;
+                if (cmd === ""){ // add empty element to keep the same height (grid layout)
+                    const emptyElement = document.createElement("div");
+                    parent.appendChild(emptyElement);
+                }
+                this.init();
+                this.execute(cmd);
+            },
+
+            execute: function(cmdArgs){
+                const cmd = cmdArgs.split(" ");
+                switch(cmd[0]){
+                    case "clear":
+                        this.clear();
+                        break;
+                    case "help":
+                        this.help();
+                        break;
+                    case "user":
+                        this.user(cmd[1]);
+                        break;
+                    default:
+                        this.error(cmdArgs);
+                        break;
+                }
+            },
+
+            clear: function(){
+                const parent = this.inputContainerElement;
+                parent.innerHTML = "";
+                this.init();
+            },
+
+            help: function(){
+                const parent = this.inputContainerElement;
+                const response = document.createElement("span");
+
+                this.removeChild();
+
+                response.style = "color: #aaa;";
+                response.innerHTML += "Available commands: clear help";
+                parent.appendChild(response);
+                this.init();
+            },
+
+            error: function(cmd, arg){
+                const parent = this.inputContainerElement;
+                const response = document.createElement("span");
+
+                this.removeChild();
+
+                response.style = "color: #f00;";
+                response.innerHTML += "Error: command not found: " + cmd + (arg  ? " " + arg : "");
+                parent.appendChild(response);
+                this.init();
+            },
+
+            removeChild: function(){
+                const parent = this.inputContainerElement;
+                const oldchild = this.inputContainerElement.querySelector("input");
+                parent.removeChild(oldchild);
+            },
+
+            user: function(username){
+                if (username === undefined){
+                    this.error("user");
+                } else {
+                    this.removeChild();
+                    const blank = document.createElement("p");
+                    this.inputContainerElement.appendChild(blank);
+                    this.init(username); // TODO fix grid layout, se il nome è più corto la griglia non cambia
+                }
             }
         };
-
         terminal.init()
-        // console.log(terminal.inputContainerElement)
-        // inizializza la shell
-    }
 
+    }
 })()
+
