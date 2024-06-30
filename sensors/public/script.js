08fe0489fb55aa1b5ad1923892543070310f5a6b
Picred <andry4you@hotmail.it>
2024-06-30 19:20:41 +0200

    feat: login, add new sensors

--- sensors/public/script.js
+++ sensors/public/script.js
@@ -1,11 +1,82 @@
 (() => {
     window.onload = () => {
-        const mainAppContainer = document.querySelector(".desktop>.main-content>.apps-container");
-
-        mainAppContainer.innerHTML += "<div class='main-app'>app3</div>";
-
         const socket = io();
+        const desktop = document.querySelector(".desktop");
+        const loginFormContainer = document.querySelector(".loginform-container");
+        const loginBtn = document.querySelector(".loginform-container>.loginform>.login-btn");
 
+        loginBtn.addEventListener("click", (e) => {
+            const username = loginFormContainer.querySelector(".loginform>.username").value;
+            const psw = loginFormContainer.querySelector(".loginform>.psw").value;
+            
+            if(!username || !psw){
+                alert("Fill all fields");
+            }
+            else{
+                const userData = {
+                    username: username,
+                    psw: psw
+                }
+                loginUser(socket, userData);
+            }
+        })
+
+        const loginUser = (socket, userData) => {
+            socket.emit("login", userData);
+
+            socket.on("userLogged", () => {
+                alert("Logged in :)");
+
+                desktop.style.display = "grid";
+                loginFormContainer.style.display = "none";
+
+            });
+
+            socket.on("incorrectPsw", () => {
+                alert("Incorrect Password");
+            });
+
+            socket.on("userRegistered", () => {
+                alert("Registered");
+                desktop.style.display = "grid";
+                loginFormContainer.style.display = "none";
+            })
+        }
+
+        // Add new sensor
+        const addSensorBtn = document.querySelector(".desktop>.sidebar>.sidebar-elem>.add-sensor-btn");
         
+        addSensorBtn.addEventListener("click", () => {
+            const sensorType = document.querySelector(".desktop>.sidebar>.sidebar-elem>.sensor-type").value;
+            const sensorName = document.querySelector(".desktop>.sidebar>.sidebar-elem>.sensor-name").value;
+            const sensorUpdateFreq = document.querySelector(".desktop>.sidebar>.sidebar-elem>.sensor-update-frequency").value;
+
+            if(!sensorName || !sensorType || !sensorUpdateFreq)
+                alert("Fill all fields");
+            else{
+                const sensorData = {
+                    name: sensorName,
+                    type: sensorType,
+                    updateFreq: sensorUpdateFreq
+                }
+                socket.emit("newSensor", sensorData);
+            }
+        })
+
+        // Show my sensors from server
+        socket.on("mySensors", function(mySensors){
+            for (sensor of mySensors){
+                // create html element "sensor" and append to sensorsContainer
+                const sensorsContainer = document.querySelector(".desktop>.main-content>.sensors-container");
+                sensorsContainer.innerHTML = "";
+                const newSensor = document.createElement("div");
+                newSensor.classList = ["sensor"];
+                newSensor.innerHTML = `<p class='name'>${sensor.name}</p>
+                                    <p class='type'>${sensor.type}</p>
+                                    <p class='updateFreq'>${sensor.updateFreq}</p>`;
+                sensorsContainer.appendChild(newSensor);
+                // TODOOOOOOO
+            }
+        })
     }
 })();
--- sensors/server.js
+++ sensors/server.js
@@ -7,14 +7,84 @@ app.use(express.static("./public"));
 app.use(express.urlencoded({extent:false}));
 app.use(express.json());
 
+const databaseUsers = {};
+const userSensors = {}; 
+/*
+userSensors = {
+    "username" : [
+        {
+            "sensorName" : sensorName,
+            "sensorType" : sensorType,
+            "sensorUpdateFreq" : sensorUpdateFreq
+        },
+        {
+            "sensorName" : sensorName,
+            "sensorType" : sensorType,
+            "sensorUpdateFreq" : sensorUpdateFreq
+        }
+    ],
+    "username2" : [{...}]
+}
+*/
 
 io.on("connection", function(socket) {
     console.log("New user connected");
+    let userRcv = {};
+    socket.on("login", function(data) {
+        userRcv = setUser(data);
 
-    socket.on("event", function(data) {
-        console.log("Ricevuto" + data);
-    })
-})
+
+        if(isRegistered(userRcv))
+            if(isCorrectPsw(userRcv))
+                socket.emit("userLogged");
+            else
+                socket.emit("incorrectPsw");
+        else{
+            databaseUsers[userRcv.username] = userRcv.psw;
+            userSensors[userRcv.username] = [];
+            socket.emit("userRegistered");
+        }
+        socket.emit("mySensors", userSensors[userRcv.username]);
+    });
+
+    socket.on("newSensor", function(sensorData){
+        console.log("User " + userRcv.username + " requests adding new sensor: " + JSON.stringify(sensorData));
+        
+        const newSensor = {
+            name : sensorData.name,
+            type : sensorData.type,
+            updateFreq : sensorData.updateFreq
+        };
+
+        userSensors[userRcv.username].push(newSensor);
+
+
+        console.log("\n\n UserSensors:  " + JSON.stringify(userSensors));
+
+        socket.emit("mySensors", userSensors[userRcv.username]);
+    });
+
+
+
+
+});
+
+const setUser = (data) => {
+    const user = {
+        username: data.username,
+        psw: data.psw
+    };
+    return user;
+}
+
+const isRegistered = (userRcv) => {
+    return databaseUsers[userRcv.username];
+}
+
+const isCorrectPsw = (userRcv) => {
+    return databaseUsers[userRcv.username] == userRcv.psw;
+}
+
 
 http.listen(8080, () => {
     console.log("Server Listening on port 8080");
