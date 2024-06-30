dc7365b927a7c2e714fe19226c8aa816df5bc083
Picred <andry4you@hotmail.it>
2024-06-30 10:59:55 +0200

    feat: login form

--- sensors/public/script.js
+++ sensors/public/script.js
@@ -3,5 +3,9 @@
         const mainAppContainer = document.querySelector(".desktop>.main-content>.apps-container");
 
         mainAppContainer.innerHTML += "<div class='main-app'>app3</div>";
+
+        const socket = io();
+
+        
     }
 })();
